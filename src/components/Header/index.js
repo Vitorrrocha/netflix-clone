3322ea028f12716dff419799160fff8bdc896212
Vitor Augusto Batista Rocha <44412471+Vitorrrocha@users.noreply.github.com>
2021-05-10 12:28:36 -0300

    Feat: Update logo.png

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default ({black}) => {
     return(
         <header className={black ?"black" : ''}>
             <div className='header-logo'>
-                <a href='/'><img alt='netflix logo' src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"/></a>
+                <a href='/'><img alt='netflix logo' src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/64-netflix-512.png"/></a>
             </div>
             <div className='header-user'>
                 <a href='/'><img alt='user'src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/></a>
